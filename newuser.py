11c395a85bde3667708415002dc8fcbc5eedf43a
Greg Ayling <aylinggreg@gmail.com>
2012-05-25 10:23:36 -0400

    newuser script

---
+++ newuser.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+import getpass, base64, pymongo
+
+username = raw_input("Enter your Jobmine username: ") 
+password = base64.b64encode(getpass.getpass())
+email = raw_input("Enter your preferred email address: ")
+
+db = pymongo.Connection('localhost', 27017).jobminetest
+users = db.users
+user = users.find_one({'user':username})
+if user == None:
+  users.insert({'user':username, 'password':password, 'email':email, 'active':1})
+  print username + ' inserted into the database'
+else:
+  print username + ' already exists in the database!'
